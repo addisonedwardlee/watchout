c0f881a399c7ed0d2c8ca1ae396a296234d1c369
Hack Reactor Students <addisonedwardlee@gmail.com>
2014-03-07 15:01:01 -0800

    Draws enemies in the svg element

--- watchout.js
+++ watchout.js
@@ -1,2 +1,14 @@
 // start slingin' some d3 here.
+for(var i=0; i<10; i++){
+d3.select('svg').append("circle")
+  .attr("class", "enemy")
+  .attr("cx", function(){ return Math.random()*700})
+  .attr("cy", function(){ return Math.random()*450})
+  .attr("color", "black")
+  .attr("r", 10);
+}
 
+
+
+
+// data(data, function(d){return d;d3.range(10));
