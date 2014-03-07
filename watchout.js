94057499c1e9767c94b02b80d5456800992fd46e
Hack Reactor Students <addisonedwardlee@gmail.com>
2014-03-07 15:03:22 -0800

    Circles now works without using a for-loop

--- watchout.js
+++ watchout.js
@@ -1,12 +1,11 @@
 // start slingin' some d3 here.
-for(var i=0; i<10; i++){
-d3.select('svg').append("circle")
+d3.select('svg').selectAll('circle').data(d3.range(15))
+  .enter().append("circle")
   .attr("class", "enemy")
   .attr("cx", function(){ return Math.random()*700})
   .attr("cy", function(){ return Math.random()*450})
   .attr("color", "black")
   .attr("r", 10);
-}
 
 
 
