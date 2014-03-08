588793f4bb48816082355b7984c38a602337e7c9
Hack Reactor Students <addisonedwardlee@gmail.com>
2014-03-08 12:14:01 -0800

    refactored code and new features

--- watchout.js
+++ watchout.js
@@ -1,6 +1,9 @@
 /* global d3 */
+//create the things
+var svg = d3.select("svg");
 
-d3.select("svg").append("svg:image")
+//add hero
+svg.append("svg:image")
   .attr("xlink:href", "bear.png")
   .attr("class", "hero")
   .attr("width", 30)
@@ -8,17 +11,19 @@ d3.select("svg").append("svg:image")
   .attr("x", 350)
   .attr("y", 225);
 
-d3.select("svg").append("svg:image")
+//add honey
+svg.append("svg:image")
   .attr({
     "class": "honey",
     "xlink:href": "honey.png",
     "width": 40,
     "height": 60,
-    "x": function(){ return Math.random()*700;},
-    "y": function(){ return Math.random()*450;},
+    "x": function(){ return 30 + Math.random()*640;},
+    "y": function(){ return 30 + Math.random()*390;},
 });
 
-d3.select("svg").selectAll(".enemy").data(d3.range(20))
+//add enemy
+svg.selectAll(".enemy").data(d3.range(10))
   .enter()
   .append("svg:image")
   .attr("class", "enemy")
@@ -30,31 +35,40 @@ d3.select("svg").selectAll(".enemy").data(d3.range(20))
     "y": function(){ return Math.random()*450;},
 });
 
+//assign references to the things
+var hero = d3.select(".hero");
+var heroWidth = hero.attr("width");
+var heroHeight = hero.attr("height");
+var honey = d3.select(".honey");
+var honeyWidth = honey.attr("width");
+var honeyHeight = honey.attr("height");
+var allEnemies = d3.selectAll(".enemy");
+
 //move hero function
-d3.select("svg").on("mousemove", function(){
-  d3.select(".hero").attr("x", function(){
-    var x = d3.mouse(this)[0];
-    if(x < 670 && x > 30){
+svg.on("mousemove", function(){
+  hero.attr("x", function(){
+    var x = d3.mouse(this)[0] - 0.5 * heroWidth;
+    if(x < (700-heroWidth) && x > 0){
       return x;
-    } else if (x > 670) {
-      return 670;
+    } else if (x > (700-heroWidth)) {
+      return (700-heroWidth);
     } else {
-      return 30;
+      return 0;
     }
   }).attr("y", function(){
-    var y = d3.mouse(this)[1];
-    if(y < 420 && y > 30){
+    var y = d3.mouse(this)[1]- 0.5 * heroHeight;
+    if(y < (450-heroHeight) && y > 0){
       return y;
-    } else if (y > 420) {
-      return 420;
+    } else if (y > (450-heroHeight)) {
+      return (450-heroHeight);
     } else {
-      return 30;
+      return 0;
     }
   });
 });
 
 var moveEnemy = function(){
-  d3.selectAll(".enemy")
+  allEnemies
   .transition()
   .attr("x", function(){ return Math.random()*700;})
   .attr("y", function(){ return Math.random()*450;})
@@ -64,9 +78,9 @@ var moveEnemy = function(){
 };
 
 var collisionDetected = function(){
-  var heroX = d3.select('.hero').attr("x");
-  var heroY = d3.select('.hero').attr("y");
-  var allEnemy = d3.selectAll('.enemy')[0];
+  var heroX = hero.attr("x");
+  var heroY = hero.attr("y");
+  var allEnemy = allEnemies[0];
   for(var i = 0; i < allEnemy.length; i++){
     var enemyX = allEnemy[i].x.baseVal.value;
     var enemyY = allEnemy[i].y.baseVal.value;
@@ -78,10 +92,10 @@ var collisionDetected = function(){
 };
 
 var honeyDetected = function(){
-  var heroX = d3.select('.hero').attr("x");
-  var heroY = d3.select('.hero').attr("y");
-  var honeyX = d3.select('.honey').attr("x");
-  var honeyY = d3.select('.honey').attr("y");
+  var heroX = hero.attr("x") - 0.5 * heroWidth;
+  var heroY = hero.attr("y") - 0.5 * heroHeight;
+  var honeyX = honey.attr("x") - 0.5 * honeyWidth;
+  var honeyY = honey.attr("y") - 0.5 * honeyHeight;
   if(Math.abs(honeyX - heroX) < 20 && Math.abs(honeyY - heroY) < 20){
     return true;
   }
@@ -96,17 +110,29 @@ var collisionCheck = function(){
       parseInt(d3.select('#highScore').text(curScore));
     }
     d3.select('#curScore').text("0");
+    d3.select('.playField').classed({
+      "collided": true,
+      "safe": false
+    });
+    setTimeout(function(){
+      d3.select('.playField').classed({
+        "collided": false,
+        "safe": true
+      });
+    }, 100);
   }else{
     d3.select('#curScore').text(curScore+1);
   }
   if(honeyDetected()){
     d3.select('#curScore').text(curScore+100);
-    d3.select('.honey').attr({
-      "x": function(){ return Math.random()*700;},
-      "y": function(){ return Math.random()*450;}
-    });
+    honey.attr({
+      "x": function(){ return 30 + Math.random()*640;},
+      "y": function(){ return 30 + Math.random()*390;}
+    })
+    heroHeight+1;
+    heroWidth+2;
   }
-  setTimeout(collisionCheck, 500);
+  setTimeout(collisionCheck, 20);
 };
 
 moveEnemy();
